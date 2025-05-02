93bac02ac33479502b2f31800f490ecf045f4b8f
aidancomiskey <137325474+aidancomiskey@users.noreply.github.com>
2025-05-02 14:26:54 -0400

    Post-review update solvent_correlation.py
    Expanded regression analysis to include lnK response variable, additional predictors

--- solvent_correlation.py
+++ solvent_correlation.py
@@ -2,9 +2,11 @@ import pandas as pd
 import statsmodels.api as ssm
 from scipy import stats
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-from plot import plot_multivariate
+from plot import plot_multivariate, plot_pred_vs_actual
 from sklearn.preprocessing import StandardScaler
+from math import log
 
+DEFAULT_LN_K = True
 SOLVENT_PARAMETERS = "Data/solvent_params.csv"
 BORONIC_ACIDS = ["PBA", "ompPBA"]
 DIOLS = ["Alizarin", "Catechol", "Hydrobenzoin"]
@@ -17,15 +19,23 @@ solvent_param_df = pd.read_csv(SOLVENT_PARAMETERS, index_col=0)
 
 
 # Performs linear regressions by boronic acid-diol pair for a single solvent parameter (predictor)
-def single_regression(titration_group, predictor, print_results=False):
+# NOTE: right now, K or ln_K is chosen by default setting for ln_K param, control from main.py
+def single_regression(titration_group, predictor, ln_K=DEFAULT_LN_K, print_results=False):
     result_dict = {}
     for pair, experiments in zip(BINDING_PAIRS, EXPERIMENT_LISTS):
         titrations = [titration_group[exp] for exp in experiments]
         solvents = [titration.solvent for titration in titrations]
         K_values = [titration.output_K for titration in titrations]
-        regression_df = pd.DataFrame({"solvent": solvents, "K_values": K_values})
+        lnK_values = [log(titration.output_K) for titration in titrations]
+        if ln_K:
+            response_key = "lnK_values"
+            regression_df = pd.DataFrame({"solvent": solvents, response_key: lnK_values})
+        else:
+            response_key = "K_values"
+            regression_df = pd.DataFrame({"solvent": solvents, response_key: K_values})
+
         regression_df[predictor] = regression_df["solvent"].map(solvent_param_df[predictor])
-        result = stats.linregress(x=regression_df[predictor], y=regression_df["K_values"])
+        result = stats.linregress(x=regression_df[predictor], y=regression_df[response_key])
         result_dict[pair] = {f"coefficient_{predictor}": result.slope,
                              f"r_squared_{predictor}": result.rvalue ** 2,
                              f"p_value_{predictor}": result.pvalue}  # Choose which info to report in Dataframe
@@ -36,19 +46,26 @@ def single_regression(titration_group, predictor, print_results=False):
 
 
 # Performs multivariate linear regressions by boronic acid-diol pair for two solvent parameters (predictors)
-def multi_regression(titration_group, predictors, standardize=True, report_full_model=False, plot_3D=False,
-                     plot_style="3d", print_results=False):
+# NOTE: right now, K or ln_K is chosen by default setting for ln_K param, control from main.py
+def multi_regression(titration_group, predictors, ln_K=DEFAULT_LN_K, standardize=True, report_full_model=False, plot_3D=False,
+                     plot_style="3d", plot_predicted_actual=False, print_results=False):
     p1, p2 = predictors
     result_dict = {}
     for pair, experiments in zip(BINDING_PAIRS, EXPERIMENT_LISTS):
         titrations = [titration_group[exp] for exp in experiments]
         solvents = [titration.solvent for titration in titrations]
         K_values = [titration.output_K for titration in titrations]
-        regression_df = pd.DataFrame({"solvent": solvents, "K_values": K_values})
+        lnK_values = [log(titration.output_K) for titration in titrations]
+        if ln_K:
+            response_key = "lnK_values"
+            regression_df = pd.DataFrame({"solvent": solvents, response_key: lnK_values})
+        else:
+            response_key = "K_values"
+            regression_df = pd.DataFrame({"solvent": solvents, response_key: K_values})
         regression_df[p1] = regression_df["solvent"].map(solvent_param_df[p1])
         regression_df[p2] = regression_df["solvent"].map(solvent_param_df[p2])
 
-        y = regression_df["K_values"]
+        y = regression_df[response_key]
         X = regression_df[[p1, p2]]
 
         # Standardize X data by converting to Z score
@@ -91,18 +108,34 @@ def multi_regression(titration_group, predictors, standardize=True, report_full_
         if plot_3D:
             reduced_y = y/1e4
             title = f"{pair[0]} & {pair[1]}"
+            #NOTE: ln(K) values aren't "reduced" during contour plotting -- done, but not via argument
             plot_multivariate(X=X,
-                              y=reduced_y,
+                              y=y,
                               p1=p1,
-                              p1_coeff=result.params[p1] / 1e4,
+                              p1_coeff=result.params[p1],
                               p2=p2,
-                              p2_coeff=result.params[p2] / 1e4,
-                              intercept=result.params["const"] / 1e4,
-                              y_label="Affinity (×10⁴)",
+                              p2_coeff=result.params[p2],
+                              intercept=result.params["const"],
+                              #y_label="Affinity (×10⁴)",
+                              y_label=response_key,
                               title=title,
                               filename=f"Plots/Model/{plot_style}/{title}.png",
                               style=plot_style,
                               standardize=standardize)
+        if plot_predicted_actual:
+            pair_text = f"{pair[0]} & {pair[1]}"
+            plot_pred_vs_actual(X=X,
+                                y=y,
+                                p1=p1,
+                                p1_coeff=result.params[p1],
+                                p2=p2,
+                                p2_coeff=result.params[p2],
+                                intercept=result.params["const"],
+                                response_variable=response_key,
+                                pair_text=pair_text,
+                                r_squared=result.rsquared,
+                                legend=solvents)
+
         if print_results:
             print(f"Pair: {pair[0]} & {pair[1]}")
             print(result.summary())
